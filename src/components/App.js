bffc812a57657fb4e4d060a66fa90049e0aad7a0
Sudipa Acharya <sudipaacharya351@gmail.com>
2022-12-27 04:06:05 +0530

    code added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "./../styles/App.css";
 
-class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+function App() {
+  const [displayMessage, setDisplayMessage] = useState(false);
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  return (
+    <div id="main">
+      {displayMessage === false ? null : (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
+      <button id="click" onClick={() => setDisplayMessage(true)}>
+        Click me
+      </button>
+    </div>
+  );
 }
 
-
 export default App;
-
